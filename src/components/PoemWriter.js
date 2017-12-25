2674145f184300d752f2406174e1e991efd93f57
Jason Vance <jbvance@gmail.com>
2017-12-25 14:32:38 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -5,12 +5,29 @@ class PoemWriter extends React.Component {
     super(props);
 
     this.state = {};
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea
+          rows="3"
+          cols="60"
+          name="poemText"
+          value={this.state.poemText}
+          onChange={this.handleInputChange}
+        />
         <div id="poem-validation-error" style={{ color: 'red' }}>
           This poem is not written in the right structure!
         </div>
